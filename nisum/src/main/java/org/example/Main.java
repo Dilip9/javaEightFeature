6f45b08423d987fa5af0a88989697b2a26e077c3
Dilip9 <dilip.verma940805@gmail.com>
2025-03-04 02:19:29 +0530

    Initial Commit for java Stream API

---
+++ nisum/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
